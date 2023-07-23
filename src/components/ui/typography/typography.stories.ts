e0a479e9b157120d8b3fbb32525eb6c60d678cf4
SergShcherbina <1990sergik27@gmail.com>
2023-07-24 01:32:09 +0300

    add component typography

---
+++ src/components/ui/typography/typography.stories.ts
@@ -0,0 +1,119 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Typography } from "./";
+
+const meta = {
+  title: "Components/Typography",
+  component: Typography,
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      options: [
+        "large",
+        "h1",
+        "h2",
+        "h3",
+        "body1",
+        "subtitle1",
+        "body2",
+        "subtitle2",
+        "caption",
+        "overline",
+        "link1",
+        "link2",
+      ],
+      control: { type: "radio" },
+    },
+  },
+} satisfies Meta<typeof Typography>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Large: Story = {
+  args: {
+    children: "Large",
+    variant: "large",
+  },
+};
+
+export const H1: Story = {
+  args: {
+    children: "h1",
+    variant: "h1",
+  },
+};
+
+export const H2: Story = {
+  args: {
+    children: "h2",
+    variant: "h2",
+  },
+};
+
+export const H3: Story = {
+  args: {
+    children: "h3",
+    variant: "h3",
+  },
+};
+
+export const Body1: Story = {
+  args: {
+    children: "body1",
+    variant: "body1",
+  },
+};
+
+export const SubTitle1: Story = {
+  args: {
+    children: "subtitle1",
+    variant: "subtitle1",
+  },
+};
+
+export const Body2: Story = {
+  args: {
+    children: "body2",
+    variant: "body2",
+  },
+};
+
+export const Subtitle2: Story = {
+  args: {
+    children: "subtitle2",
+    variant: "subtitle2",
+  },
+};
+
+export const Caption: Story = {
+  args: {
+    children: "caption",
+    variant: "caption",
+  },
+};
+
+export const Overline: Story = {
+  args: {
+    children: "overline",
+    variant: "overline",
+  },
+};
+
+export const TypographyAsLink_1: Story = {
+  args: {
+    children: "link_1",
+    variant: "link1",
+    href: "#",
+    as: "a",
+  },
+};
+
+export const TypographyAsLink_2: Story = {
+  args: {
+    children: "link_2",
+    variant: "link2",
+    href: "#",
+    as: "a",
+  },
+};
---
+++ src/components/ui/typography/typography.tsx
@@ -0,0 +1,29 @@
+import { ComponentPropsWithoutRef, ElementType } from "react";
+
+import s from "./typography.module.scss";
+
+export type TypographyProps<T extends ElementType = "p"> = {
+  as?: T;
+  className?: string;
+  variant?:
+    | "large"
+    | "h1"
+    | "h2"
+    | "h3"
+    | "body1"
+    | "subtitle1"
+    | "body2"
+    | "subtitle2"
+    | "caption"
+    | "overline"
+    | "link1"
+    | "link2";
+} & ComponentPropsWithoutRef<T>;
+
+export const Typography = <T extends ElementType = "p">(
+  props: TypographyProps<T>,
+) => {
+  const { className, variant = "body1", as: Component = "p", ...rest } = props;
+
+  return <Component className={`${s[variant]} ${className}`} {...rest} />;
+};
