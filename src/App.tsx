7fdc68ab3bd50135ffd5074a713eb1485e698a67
AlexanderPanevkin <panevkin900@gmail.com>
2023-07-28 18:43:14 +0300

    fix: app error

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
 export function App() {
-  return (
-    <div>
-      <div>testing git words</div>
-      Hello 111!
-    </div>
-    <>
-      <div>Hello 111!</div>
-      <div>Hello 222!</div>
-    </>
-  )
+  return <div>Hello 111!</div>
 }
