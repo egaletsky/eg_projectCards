348f93d141005ddb6be16a391d7a939940bfcdcd
SergShcherbina <1990sergik27@gmail.com>
2023-07-30 21:21:00 +0300

    feat: add component Avatar

---
+++ src/components/ui/avatar/avatar.tsx
@@ -0,0 +1,26 @@
+import { FC } from 'react'
+
+import * as AvatarRadix from '@radix-ui/react-avatar'
+
+import s from './avatar.module.scss'
+
+type AvatarType = {
+  src?: string
+  size?: number
+}
+
+export const Avatar: FC<AvatarType> = ({ src, size }) => {
+  return (
+    <div>
+      <AvatarRadix.Root
+        className={s.avatarRoot}
+        style={{ height: `${size}px `, width: `${size}px` }}
+      >
+        <AvatarRadix.Image className={s.avatarImage} src={src} alt="User photo" />
+        <AvatarRadix.Fallback className={s.avatarFallback} delayMs={600}>
+          AVA
+        </AvatarRadix.Fallback>
+      </AvatarRadix.Root>
+    </div>
+  )
+}
